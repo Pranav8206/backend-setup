49517585bbdfcc3301dbb2f5c2aabbc233191d04
Pranav8206 <pranavmawle8206@gmail.com>
2025-08-12 23:22:56 +0530

    utils folder updated

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import connectDB from "./db/index.js";
-
-
+import { app } from "./app.js";
 
 connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log(err);
+      throw err;
+    });
 
+    app.get("/", (req, res) => {
+      res.send("pranav");
+      console.log("PM");
+    });
 
-
-
-
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server listening at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
 
 // Approach 1:-
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => { () => {}}
+// const asyncHanlder = (func) => () => {}
+// const asyncHanlder = (func) => async() => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             status : false,
+//             message : error.message
+//         })
+
+//     }
+// }
